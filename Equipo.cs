2c6e8cdb63c60635c098a9725845946efa2fb74f
lmmolinabe92 <lmmolinabe92@gmail.com>
2023-03-01 18:20:02 +0100

    Agregar archivos de proyecto.

---
+++ Equipo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    public class Equipo
+    {
+        private string nombre;
+        private string universidad;
+        private string lenguage;
+        private List<Programador> miembros;
+
+        public Equipo(string nombre, string universidad, string lenguage)
+        {
+            this.nombre = nombre;
+            this.universidad = universidad;
+            this.lenguage = lenguage;
+        }
+
+        public bool equipoCompleto()
+        {
+            return miembros.Count == 3;
+        }
+
+        public void anyadirProgramador(Programador p)
+        {
+            try
+            {
+                if (equipoCompleto())
+                {
+                    throw new Exception("Equipo lleno");
+                }
+                miembros.Add(p);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+
+    public class Programador
+    {
+        private string nombre;
+        private string apellidos;
+
+        public Programador(string nombre, string apellidos)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+        }
+
+        public string Nombre
+        {
+            get => nombre; set
+            {
+                if (!value.All(char.IsLetter) || value.Length >= 20)
+                {
+                    throw new Exception("Nombre incorrecto");
+                }
+                nombre = value;
+            }
+        }
+        public string Apellidos
+        {
+            get => apellidos; set
+            {
+                if(!value.All(char.IsLetter) || value.Length >= 20)
+                {
+                    throw new Exception("Apellidos incorrectos");
+                }
+                apellidos = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entra un numero");
+            double n= double.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Logaritmo: " + CalculosNumericos.Logaritmo(n));
+                Console.WriteLine("Raiz Cuadrada: " + CalculosNumericos2.RaizCuadrada(n));
+            }
+            catch (Exception ex) { }
+            Console.ReadLine();
+        }
+    }
+
+    public class CalculosNumericos {
+        public static double Logaritmo(double n) {
+            if (n < 0) {
+                throw new ArithmeticException();
+            }
+            return Math.Log(n);
+        }
+    }
+
+    public class CalculosNumericos2 {
+        public static double RaizCuadrada(double n) {
+            if (n < 0)
+            {
+                throw new ArithmeticException();
+            }
+            return Math.Sqrt(n);
+        }
+    }
+}
